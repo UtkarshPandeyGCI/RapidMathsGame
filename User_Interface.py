75b7a3aa647dcc34b5a9bb112804cc259c5c97a8
Utkarsh Pandey <35457627+UtkarshPandeyGCI@users.noreply.github.com>
2021-12-29 15:22:53 +0530

    Bug fixed for 3.3333333 != 3.33 as per rule to provide points
    when a question use to arrive with its answer as 6.6666666666 and user use to enter 6.66 as the rule says to enter till 2 decimal places, points were not given for the answer so extra elif condition added.

--- User_Interface.py
+++ User_Interface.py
@@ -24,11 +24,12 @@ def Welcome_user():
                                 Put Your Best Efforts and Make High Scores!
 
 """)
-    time.sleep(10) #Time provided to read the game info
+    #time.sleep(10) #Time provided to read instructions | removed on 29-12-21
+    input("Press any key to continue...")
     print('\r')
     
     username = input("Enter your name: ")
-    time.sleep(2)
+    #time.sleep(2) | removed on 29-12-21
     print('\r')
     print("\n\t\tSolve the following questions\n")
 
@@ -42,6 +43,8 @@ def Welcome_user():
         #check for quiet
         if user_input_answer == 'done':
             break
+        elif user_input_answer == '':
+            user_input_answer = '0.0'
         else:
             user_input_answer = float(user_input_answer)
             pass
@@ -49,14 +52,25 @@ def Welcome_user():
         #check
         evaluated = eval(question)
         
-        if round(evaluated,2) == round(user_input_answer,2):
+        #if round(evaluated,2) == round(user_input_answer,2): || REMOVED on December 29, 2021 || Reason: Bug fixed for 3.3333333 != 3.33 as per rule to provide points
+            #print(type(evaluated),type(user_input_answer))
+            #print(evaluated,user_input_answer)
+        
+        if evaluated == user_input_answer:
             f=functions.ScorePoints.score.append(1) #+1 point
+            print("Computer's calculation: ",evaluated,"\tYour answer: ",user_input_answer)
             pass
+        elif str(evaluated)[:4]==str(user_input_answer)[:4]:
+            f=functions.ScorePoints.score.append(1) #+1 point
+            print("Computer's calculation: ",evaluated,"\tYour answer: ",user_input_answer)
+            pass
+        
         else:
+            print("Computer's calculation: ",evaluated,"\tYour answer: ",user_input_answer)
             f=functions.ScorePoints.score.append(0) #NO points
             pass
 
-        print("You receive :",functions.ScorePoints.score[-1]," point")
+        print("You received a total of : ",functions.ScorePoints.score[-1]," points")
         #record_book
         solution_evaluated_n_input = ((evaluated),(user_input_answer))
         functions.Session.qna_record[question] = solution_evaluated_n_input
@@ -64,9 +78,10 @@ def Welcome_user():
     obtained = sum(functions.ScorePoints.score)
     total = count-1
     print(f"\n{username} scored {obtained} points out of {total}\n")
-    #print(functions.Session.qna_record)
-    #print(functions.ScorePoints.score)
-    print(functions.report())
+    print(functions.report_tab())
+
+if '__init__' == '__main__':
     
-Welcome_user()
-input()
+    Welcome_user()
+    input()
+    
